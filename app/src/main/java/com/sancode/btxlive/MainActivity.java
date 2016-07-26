9e664e68bb2ce5966ff71bb8ac1df2c35b613924
Karina Betzabe Romero Ulloa <betzy_skamm@hotmail.com>
2016-07-26 13:19:34 -0700

    Initial commit

---
+++ app/src/main/java/com/sancode/btxlive/MainActivity.java
@@ -0,0 +1,209 @@
+package com.sancode.btxlive;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Point;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager.LayoutParams;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.webrtc.MediaStream;
+import org.webrtc.VideoRenderer;
+import org.webrtc.VideoRendererGui;
+
+import java.util.List;
+
+import de.tavendo.autobahn.WebSocketException;
+
+public class MainActivity extends Activity implements WebRtcClient.RtcListener{
+
+    private final static int VIDEO_CALL_SENT = 666;
+    private static final String VIDEO_CODEC_VP9 = "VP9";
+    private static final String AUDIO_CODEC_OPUS = "opus";
+    // Local preview screen position before call is connected.
+    private static final int LOCAL_X_CONNECTING = 0;
+    private static final int LOCAL_Y_CONNECTING = 0;
+    private static final int LOCAL_WIDTH_CONNECTING = 100;
+    private static final int LOCAL_HEIGHT_CONNECTING = 100;
+    // Local preview screen position after call is connected.
+    private static final int LOCAL_X_CONNECTED = 72;
+    private static final int LOCAL_Y_CONNECTED = 72;
+    private static final int LOCAL_WIDTH_CONNECTED = 25;
+    private static final int LOCAL_HEIGHT_CONNECTED = 25;
+    // Remote video screen position
+    private static final int REMOTE_X = 0;
+    private static final int REMOTE_Y = 0;
+    private static final int REMOTE_WIDTH = 100;
+    private static final int REMOTE_HEIGHT = 100;
+    private VideoRendererGui.ScalingType scalingType = VideoRendererGui.ScalingType.SCALE_ASPECT_FILL;
+    private GLSurfaceView vsv;
+    private VideoRenderer.Callbacks localRender;
+    private VideoRenderer.Callbacks remoteRender;
+    private WebRtcClient client;
+    private String callerId;
+    private String wsuri= "ws://192.168.1.75:8888";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(
+                LayoutParams.FLAG_FULLSCREEN
+                        | LayoutParams.FLAG_KEEP_SCREEN_ON
+                        | LayoutParams.FLAG_DISMISS_KEYGUARD
+                        | LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                        | LayoutParams.FLAG_TURN_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        vsv = (GLSurfaceView) findViewById(R.id.glview_call);
+        vsv.setPreserveEGLContextOnPause(true);
+        vsv.setKeepScreenOn(true);
+        VideoRendererGui.setView(vsv, new Runnable() {
+            @Override
+            public void run() {
+                init();
+            }
+        });
+
+        // local and remote render
+        remoteRender = VideoRendererGui.create(
+                REMOTE_X, REMOTE_Y,
+                REMOTE_WIDTH, REMOTE_HEIGHT, scalingType, true);
+        localRender = VideoRendererGui.create(
+                LOCAL_X_CONNECTING, LOCAL_Y_CONNECTING,
+                LOCAL_WIDTH_CONNECTING, LOCAL_HEIGHT_CONNECTING, scalingType, true);
+
+        final Intent intent = getIntent();
+        final String action = intent.getAction();
+
+        if (Intent.ACTION_VIEW.equals(action)) {
+            final List<String> segments = intent.getData().getPathSegments();
+            callerId = segments.get(0);
+            Log.d("TAG",callerId);
+        }
+    }
+
+    private void init() {
+        Point displaySize = new Point();
+        getWindowManager().getDefaultDisplay().getSize(displaySize);
+        PeerConnectionParameters params = new PeerConnectionParameters(
+                true, false, displaySize.x, displaySize.y, 30, 1, VIDEO_CODEC_VP9, true, 1, AUDIO_CODEC_OPUS, true);
+
+        try {
+            Log.d("init",params.toString());
+            client = new WebRtcClient(this, wsuri, params, VideoRendererGui.getEGLContext());
+        } catch (WebSocketException e) {
+            e.printStackTrace();
+        }
+        //clientWebRtc = new ClientWebRtc(params, listener, VideoRendererGui.getEGLContext());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        vsv.onPause();
+        if(client != null) {
+            client.onPause();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        vsv.onResume();
+        if(client != null) {
+            client.onResume();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if(client != null) {
+            client.onDestroy();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onCallReady(String callId) {
+        if (callerId != null) {
+            try {
+                answer(callerId);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else {
+            call(callId);
+        }
+    }
+
+    public void answer(String callerId) throws JSONException {
+        JSONObject jsonObject= new JSONObject();
+        jsonObject.put("type","answer");
+
+        client.sendMessage(callerId, "init", jsonObject);
+        startCam();
+    }
+
+    public void call(String callId) {
+        Intent msg = new Intent(Intent.ACTION_SEND);
+        msg.putExtra(Intent.EXTRA_TEXT, wsuri + callId);
+        msg.setType("text/plain");
+        startActivityForResult(Intent.createChooser(msg, "Call someone :"), VIDEO_CALL_SENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VIDEO_CALL_SENT) {
+            startCam();
+        }
+    }
+
+    public void startCam() {
+        // Camera settings
+        client.start("Android");
+    }
+
+    @Override
+    public void onStatusChanged(final String newStatus) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), newStatus, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onLocalStream(MediaStream localStream) {
+        localStream.videoTracks.get(0).addRenderer(new VideoRenderer(localRender));
+        VideoRendererGui.update(localRender,
+                LOCAL_X_CONNECTING, LOCAL_Y_CONNECTING,
+                LOCAL_WIDTH_CONNECTING, LOCAL_HEIGHT_CONNECTING,
+                scalingType,true);
+    }
+
+    @Override
+    public void onAddRemoteStream(MediaStream remoteStream, int endPoint) {
+        remoteStream.videoTracks.get(0).addRenderer(new VideoRenderer(remoteRender));
+        VideoRendererGui.update(remoteRender,
+                REMOTE_X, REMOTE_Y,
+                REMOTE_WIDTH, REMOTE_HEIGHT, scalingType,true);
+        VideoRendererGui.update(localRender,
+                LOCAL_X_CONNECTED, LOCAL_Y_CONNECTED,
+                LOCAL_WIDTH_CONNECTED, LOCAL_HEIGHT_CONNECTED,
+                scalingType,true);
+    }
+
+    @Override
+    public void onRemoveRemoteStream(int endPoint) {
+        VideoRendererGui.update(localRender,
+                LOCAL_X_CONNECTING, LOCAL_Y_CONNECTING,
+                LOCAL_WIDTH_CONNECTING, LOCAL_HEIGHT_CONNECTING,
+                scalingType,true);
+    }
+}
